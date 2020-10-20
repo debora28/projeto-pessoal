b8826e0bd9eefc0b147632c66d88fca36ab124cd
Debora <debrarafaela@gmail.com>
2020-10-19 22:26:31 -0300

    add

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app = require('./config/server');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5500;
 
 app.listen(port, () => console.log(`Servidor na porta ${port}`))
