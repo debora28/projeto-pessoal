e62d35d2323c253a28926b8180992a61b8532734
Debora <debrarafaela@gmail.com>
2020-10-20 00:10:45 -0300

    alter1

--- init.js
+++ init.js
@@ -6,10 +6,7 @@
     }); // end of document ready
   })(jQuery); // end of jQuery name space
   
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
+  
 
   $(document).ready(function(){
     $('.sidenav').sidenav();
